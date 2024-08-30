5b9030c5925d7799263c75faf23b66938a8a8f69
dongfanghuangjia <dongphuonghuynhgia@gmail.com>
2024-08-30 10:43:04 +0700

    update 308.1

--- code.js
+++ code.js
@@ -56,7 +56,8 @@ function btt_generate_onclick() {
         document.getElementById("txt_lyheader").value = flyfile;
     }
     let lyrpy = document.getElementById("txt_lyrpy").value;
-    document.getElementById("txt_lyrzh").value = gpinyin(lyrpy);
+    //document.getElementById("txt_lyrzh").value = gpinyin(lyrpy);
+    alert(gpinyin(lyrpy));
 }
 function str_proper(str) {
     var rstr = str.substr(0,1).toUpperCase();
@@ -75,6 +76,7 @@ function str_proper(str) {
 }
 function gpinyin(str) {
     let rstr = str.toLowerCase();
+    // Split initials
     rstr = rstr.replaceAll('b',' b');
     rstr = rstr.replaceAll('p',' p');
     rstr = rstr.replaceAll('m',' m');
@@ -90,19 +92,113 @@ function gpinyin(str) {
     rstr = rstr.replaceAll('c',' c');
     rstr = rstr.replaceAll('s',' s');
     rstr = rstr.replaceAll('h',' h');
+    rstr = rstr.replaceAll('y',' y');
+    rstr = rstr.replaceAll('w',' w');
+    // Fix zh, ch, sh initial
     rstr = rstr.replaceAll('z ','z');
     rstr = rstr.replaceAll('c ','c');
     rstr = rstr.replaceAll('s ','s');
-    rstr = rstr.replaceAll('ng','NG');
+    // Prepare any finals for n, g and r initial
+    rstr = rstr.replaceAll('ang','aNG');
+    rstr = rstr.replaceAll('an','aN');
+    rstr = rstr.replaceAll('eng','eNG');
+    rstr = rstr.replaceAll('en','eN');
+    rstr = rstr.replaceAll('ong','oNG');
+    rstr = rstr.replaceAll('ing','iNG');
+    rstr = rstr.replaceAll('in','iN');
+    rstr = rstr.replaceAll('un','uN');
+    rstr = rstr.replaceAll('ün','üN');
+    rstr = rstr.replaceAll('āng','āNG');
+    rstr = rstr.replaceAll('ān','āN');
+    rstr = rstr.replaceAll('ēng','ēNG');
+    rstr = rstr.replaceAll('ēn','ēN');
+    rstr = rstr.replaceAll('ōng','ōNG');
+    rstr = rstr.replaceAll('īng','īNG');
+    rstr = rstr.replaceAll('īn','īN');
+    rstr = rstr.replaceAll('ūn','ūN');
+    rstr = rstr.replaceAll('ǖn','ǖN');
+    rstr = rstr.replaceAll('áng','áNG');
+    rstr = rstr.replaceAll('án','áN');
+    rstr = rstr.replaceAll('éng','éNG');
+    rstr = rstr.replaceAll('én','éN');
+    rstr = rstr.replaceAll('óng','óNG');
+    rstr = rstr.replaceAll('íng','íNG');
+    rstr = rstr.replaceAll('ín','íN');
+    rstr = rstr.replaceAll('ún','úN');
+    rstr = rstr.replaceAll('ǘn','ǘN');
+    rstr = rstr.replaceAll('ǎng','ǎNG');
+    rstr = rstr.replaceAll('ǎn','ǎN');
+    rstr = rstr.replaceAll('ěng','ěNG');
+    rstr = rstr.replaceAll('ěn','ěN');
+    rstr = rstr.replaceAll('ǒng','ǒNG');
+    rstr = rstr.replaceAll('ǐng','ǐNG');
+    rstr = rstr.replaceAll('ǐn','ǐN');
+    rstr = rstr.replaceAll('ǔn','ǔN');
+    rstr = rstr.replaceAll('ǚn','ǚN');
+    rstr = rstr.replaceAll('àng','àNG');
+    rstr = rstr.replaceAll('àn','àN');
+    rstr = rstr.replaceAll('èng','èNG');
+    rstr = rstr.replaceAll('èn','èN');
+    rstr = rstr.replaceAll('òng','òNG');
+    rstr = rstr.replaceAll('ìng','ìNG');
+    rstr = rstr.replaceAll('ìn','ìN');
+    rstr = rstr.replaceAll('ùn','ùN');
+    rstr = rstr.replaceAll('ǜn','ǜN');
+    // Split n, g, r initial
     rstr = rstr.replaceAll('n',' n');
     rstr = rstr.replaceAll('g',' g');
-    rstr = rstr.replaceAll('NG','ng');
-    rstr = rstr.replaceAll(' n ','n ');
-    rstr = rstr.replaceAll(' g ','g ');
-    rstr = rstr.replaceAll(' n\n','n\n');
-    rstr = rstr.replaceAll(' g\n','g\n');
-    rstr = rstr.replaceAll('y',' y');
     rstr = rstr.replaceAll('r',' r');
+    // Fix any finals of n, g and r
+    rstr = rstr.replaceAll('aNG','ang');
+    rstr = rstr.replaceAll('aN','an');
+    rstr = rstr.replaceAll('eNG','eng');
+    rstr = rstr.replaceAll('eN','en');
+    rstr = rstr.replaceAll('eR','er');
+    rstr = rstr.replaceAll('oNG','ong');
+    rstr = rstr.replaceAll('iNG','ing');
+    rstr = rstr.replaceAll('iN','in');
+    rstr = rstr.replaceAll('uN','un');
+    rstr = rstr.replaceAll('üN','ün');
+    rstr = rstr.replaceAll('āNG','āng');
+    rstr = rstr.replaceAll('āN','ān');
+    rstr = rstr.replaceAll('ēNG','ēng');
+    rstr = rstr.replaceAll('ēN','ēn');
+    rstr = rstr.replaceAll('ēR','ēr');
+    rstr = rstr.replaceAll('ōNG','ōng');
+    rstr = rstr.replaceAll('īNG','īng');
+    rstr = rstr.replaceAll('īN','īn');
+    rstr = rstr.replaceAll('ūN','ūn');
+    rstr = rstr.replaceAll('ǖN','ǖn');
+    rstr = rstr.replaceAll('áNG','áng');
+    rstr = rstr.replaceAll('áN','án');
+    rstr = rstr.replaceAll('éNG','éng');
+    rstr = rstr.replaceAll('éN','én');
+    rstr = rstr.replaceAll('éR','ér');
+    rstr = rstr.replaceAll('óNG','óng');
+    rstr = rstr.replaceAll('íNG','íng');
+    rstr = rstr.replaceAll('íN','ín');
+    rstr = rstr.replaceAll('úN','ún');
+    rstr = rstr.replaceAll('ǘN','ǘn');
+    rstr = rstr.replaceAll('ǎNG','ǎng');
+    rstr = rstr.replaceAll('ǎN','ǎn');
+    rstr = rstr.replaceAll('ěNG','ěng');
+    rstr = rstr.replaceAll('ěN','ěn');
+    rstr = rstr.replaceAll('ěR','ěr');
+    rstr = rstr.replaceAll('ǒNG','ǒng');
+    rstr = rstr.replaceAll('ǐNG','ǐng');
+    rstr = rstr.replaceAll('ǐN','ǐn');
+    rstr = rstr.replaceAll('ǔN','ǔn');
+    rstr = rstr.replaceAll('ǚN','ǚn');
+    rstr = rstr.replaceAll('àNG','àng');
+    rstr = rstr.replaceAll('àN','àn');
+    rstr = rstr.replaceAll('èNG','èng');
+    rstr = rstr.replaceAll('èN','èn');
+    rstr = rstr.replaceAll('èR','èr');
+    rstr = rstr.replaceAll('òNG','òng');
+    rstr = rstr.replaceAll('ìNG','ìng');
+    rstr = rstr.replaceAll('ìN','ìn');
+    rstr = rstr.replaceAll('ùN','ùn');
+    rstr = rstr.replaceAll('ǜN','ǜn');
     rstr = rstr.replaceAll(String.fromCharCode(0x27),' ');
     rstr = rstr.replaceAll('  ',' ');
     return rstr.trim();
